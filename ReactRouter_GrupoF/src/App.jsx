8c2d4b9fc8b6b7ee220fdeb51aa9a2be854cdefc
Estefani Daniela Martinez <estefanidanielamartines@gmail.com>
2024-09-12 20:05:09 -0500

    scommit

--- ReactRouter_GrupoF/src/App.jsx
+++ ReactRouter_GrupoF/src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Acerca, Home, Productos, ProductosDetalle } from "./pages";
+import Menu from "./components/Menu";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [productos, setProductos] = useState([
+    {
+      id: 1,
+      nombre: "Ajiaco",
+      precio: 15000,
+    },
+    {
+      id: 2,
+      nombre: "Bandeja Paisa",
+      precio: 20000,
+    },
+    {
+      id: 3,
+      nombre: "Arroz con Pollo",
+      precio: 2000,
+    },
+    {
+      id: 4,
+      nombre: "Mondongo",
+      precio: 10000,
+    },
+  ]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <h1>Restaurante F</h1>
+      <Menu />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/acerca" element={<Acerca />} />
+        <Route path="/productos" element={<Productos productos={productos} />} />
+        <Route path="/productos/:id" element={<ProductosDetalle productos={productos} />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
