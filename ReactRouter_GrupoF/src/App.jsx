078e150366926a8937c6dde6ef1bc4e19e11632e
Eli0024 <eliana199972@gmail.com>
2024-09-19 12:28:30 -0500

    detalle producto y acerca

--- ReactRouter_GrupoF/src/App.jsx
+++ ReactRouter_GrupoF/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   const [productos, setProductos] = useState([
     { id: 1, nombre: "Ajiaco", precio: 15000, img: "https://www.bing.com/images/blob?bcid=Tn8.p0SnzoYHYsFXui.DomMyMj9i.....yQ "},
     { id: 2, nombre: "Bandeja Paisa", precio: 20000, img: "https://www.bing.com/images/blob?bcid=TiyGCGaTTIYHYsFXui.DomMyMj9i.....xI" },
-    { id: 3, nombre: "Arroz con Pollo", precio: 1200, img: "https://www.bing.com/images/blob?bcid=TjWvpN22PIYHqxcxoNWLuD9SqbotqVTdP08" },
+    { id: 3, nombre: "Arroz con Pollo", precio: 1200, img: "https://www.bing.com/images/blob?bcid=TjWvpN22PIYHYsFXui.DomMyMj9i.....4I" },
     { id: 4, nombre: "Sancocho de Gallina", precio: 20000, img: "https://cdn.theculturetrip.com/wp-content/uploads/2017/09/sancocho_de_gallina_criolla.jpg" },
     { id: 5, nombre: "Tamales", precio: 14000, img: "https://th.bing.com/th/id/R.88611189af97dc3d998c674094dc1b76?rik=bvH6haJlevMdng&riu=http%3a%2f%2fblog.redbus.co%2fwp-content%2fuploads%2f2019%2f12%2fDSC_0031-1.jpg&ehk=Q%2b%2bW5h3pNlNRUwNc87fHaO8BXhMsu5%2bep1J%2bqMEyvgU%3d&risl=&pid=ImgRaw&r=0 " },
     { id: 6, nombre: "Mojarra Frita", precio: 30000, img: "https://3.bp.blogspot.com/--_UzTo__pLg/V-yT-7Bl0QI/AAAAAAAAAGo/jD6EXai2FYAXRTctzGanpPg5h4qxwi_TgCLcB/s1600/mojarra-frita.jpg " },
